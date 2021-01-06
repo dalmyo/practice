fad7d75f1a2dfd1990e527a98ad103a471d24841
kmkimk <kmkim@aiforpet.com>
2021-01-06 18:36:49 +0900

    test

--- src/main/java/com/example/programmers/hash/PhoneNumbers.java
+++ src/main/java/com/example/programmers/hash/PhoneNumbers.java
@@ -1,7 +1,29 @@
 package com.example.programmers.hash;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 /**
  * 전화번호 목록
  */
 public class PhoneNumbers {
+
+    public static void main(String[] args) {
+
+        String[] phone_book = {"119", "97674223", "195524421"};
+
+        boolean answer = true;
+
+        ArrayList<String> list = new ArrayList<>(Arrays.asList(phone_book));
+
+        for (String s : list) {
+            long count = list.stream().filter(s1 -> s1.startsWith(s)).count();
+            if (count >= 2) {
+                answer = false;
+            }
+        }
+
+        System.out.println(answer);
+
+    }
 }
--- src/main/java/com/example/programmers/hash/Player.java
+++ src/main/java/com/example/programmers/hash/Player.java
@@ -1,12 +1,26 @@
 package com.example.programmers.hash;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 /**
  * 완주하지 못한 선수
  */
 public class Player {
 
     public static void main(String[] args) {
+        String[] participant = {"mislav", "stanko", "mislav", "ana"};
+        String[] completion = {"stanko", "ana", "mislav"};
+        String answer = "";
 
+        ArrayList<String> list = new ArrayList<>(Arrays.asList(participant));
 
+        for (String s : completion) {
+            list.remove(list.indexOf(s));
+        }
+
+        answer = list.get(0);
+
+        System.out.println(answer);
     }
 }
