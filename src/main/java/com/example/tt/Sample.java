2ff6a54f3fbc57ffe3ab1bf8e38ba0d5c9857ce2
dalmyo <dalmyo99@gmail.com>
2021-01-10 15:09:40 +0900

    test

---
+++ src/main/java/com/example/tt/Sample.java
@@ -0,0 +1,26 @@
+package com.example.tt;
+
+public class Sample {
+
+    public static void main(String[] args) {
+
+        String[] parameter = {};
+
+        String answer = "";
+
+        // 답을 출력하기 위해 구해야 하는 객체 선언
+
+
+        // 위에서 선언한 객체 세팅
+
+
+        // 제출해야하는 구조대로 자료 세팅
+
+
+        // 테스트 출력
+        System.out.println("---------- answer ----------");
+        System.out.println(answer);
+
+    }
+
+}
